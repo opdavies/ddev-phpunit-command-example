89bc76af90ef7730d2db1061a8d55a833b9c29e4
Oliver Davies <oliver@oliverdavies.uk>
2020-08-27 20:25:30 +0100

    Step 1: Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_passes()
+    {
+        $this->assertTrue(true);
+    }
+}
